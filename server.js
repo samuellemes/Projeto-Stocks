cc420efbfa7e89ebf27437f61e488cb2c051bd12
Samuel Lemes <samuellemes25@gmail.com>
2018-06-04 23:44:33 -0300

    Criando rotas.

--- server.js
+++ server.js
@@ -4,6 +4,11 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var mongooge = require('mongoose')
+var Produto = require('./app/models/produto')
+
+//URI: MLab
+mongooge.connect('mongodb://node-api:samuel123@ds247690.mlab.com:47690/node-api')
 
 //Configuração da variável app para usar o body-parser
 app.use(bodyParser.urlencoded({extend: true}))
@@ -12,13 +17,75 @@ app.use(bodyParser.json())
 //Definido porta para a execuçao da api
 var port = process.env.port || 8000
 
+//---------------------------------------ROTAS--------------------------------------------------------
+
 //Criando uma instancia das rotas via express
 var router = express.Router()
+
+router.use(function(req, res, next) {
+    console.log("Algo aqui")
+    next()
+})
 
 //Rota de exemplo
 router.get('/', function(req, res){
     res.json({message:  "Bem vindo"})
 })
+
+
+// -----------------------------------------------APIS----------------------------------------------------
+
+//Rota produtos: Utilizada para GET e POST
+router.route('/produtos')
+
+    //  Método criar:
+    .post(function(req, res) {
+        var produto = new Produto()
+
+        //Setar campos do produto
+        produto.nome = req.body.nome
+        produto.preco = req.body.preco
+        produto.descricao = req.body.descricao
+
+        produto.save(function(err) {
+            if(err) {
+                res.send('Erro ao tentar salvar produto.' + err)
+            }
+            res.json({message: 'Produto cadastrado com sucesso!'})
+        })
+    })
+
+    //Rota produtos: Utilizada para selecionar todos os produtos GET
+    .get(function(req, res) {
+        Produto.find(function(err, produtos) {
+            if(err) {
+                res.send('Erro ao tentar selecionar todos os produtos: ' + err)
+            }
+            res.json(produtos)
+        })
+    })
+
+    //Rota produtos/:produto_id (GET POST & DELETE)
+    router.route('/produtos/:produto_id')
+    
+    //Selecionar por id
+    .get(function(req, res) {
+        Produto.findById(req.params.produto_id, function(err, produto) {
+            if(err) {
+                res.send('Id do produto não encontrado: ' + err)
+            }
+            res.json(produto)
+        })
+    })
+
+    //Rota produtos/:produto_id  Atualizar por id (PUT)
+    .put(function(req, res) {
+        Produto.findById(req.params.produto_id, function(err, produto) {
+            if(err) {
+                res.send('Id do produto não encontrado: ' + err)
+            }
+        })
+    })    
 
 //Definido o padrão das rota prefixxadas com '/api'
 app.use('/api', router)
