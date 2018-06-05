aad805339d688ad7e642f54040337b5bfbd3d879
Samuel Lemes <samuellemes25@gmail.com>
2018-06-05 13:18:07 -0300

    Alterações no 'schema do produto.'

--- app/models/produto.js
+++ app/models/produto.js
@@ -6,7 +6,10 @@ var schema = mongoose.Schema
 //Produto
 var produtoSchema = new schema({
     nome: String,
+    marca: String,
+    tipo: String,
     preco: Number,
+    quantidade: Number,
     descricao: String
 })
 
